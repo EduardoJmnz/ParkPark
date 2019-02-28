519dbc46c5d423972ba5f67e469eaf25f583bfe1
EM <emmanuel.schz.hdz@gmail.com>
2019-02-28 00:27:48 -0600

    Validación primaria de dos activitys

--- app/src/main/java/com/example/eddyh/parkpark/MainActivity.java
+++ app/src/main/java/com/example/eddyh/parkpark/MainActivity.java
@@ -4,16 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText boleta;
+    EditText contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void ingresar(){
+       boleta = (EditText)findViewById(R.id.boleta);
+       contra= (EditText)findViewById(R.id.password);
+        String bol = boleta.getText().toString();
+        String pass = contra.getText().toString();
+        validaciones(bol, pass);
+    }
 
+    public void validaciones(String bol, String pass){
+        if(bol =="2016601595" && pass == "123456"){
+                Toast.makeText(this, "Bienvenido a UPIICSApark", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Usuario y contrasea incorrectos", Toast.LENGTH_LONG).show();
+        }
     }
     public void registrarse(View sender){
         Intent intent = new Intent(this, Registro.class);
--- app/src/main/java/com/example/eddyh/parkpark/Registro.java
+++ app/src/main/java/com/example/eddyh/parkpark/Registro.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
@@ -16,6 +18,19 @@ public class Registro extends AppCompatActivity {
     }
 
     public void registrar(){
+        String user = ((EditText)findViewById(R.id.user)).getText().toString(); //BOLETA
+        String nombre = ((EditText)findViewById(R.id.Nombre)).getText().toString();
+        String pass1 = ((EditText)findViewById(R.id.password)).getText().toString();
+        String pass2 =  ((EditText)findViewById(R.id.confirmarPass)).getText().toString();
+
+        if(user != ""  && nombre != "" && pass1 == pass2){
+            Toast.makeText(this,"Usuario registrado, espera por validación de datos por parte de Subdirección Escolar", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"Aún faltan campos por llenar", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
     public void cancelar(View sender){
