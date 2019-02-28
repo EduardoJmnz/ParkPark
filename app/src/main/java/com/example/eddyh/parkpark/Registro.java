aba0cff32626fdae22c18f3687f745246615b07b
Eddy <eddy.jimenz@yahoo.com>
2019-02-27 23:16:26 -0600

    Activity donde se va a mostrar los estacionamientos

--- app/src/main/java/com/example/eddyh/parkpark/Registro.java
+++ app/src/main/java/com/example/eddyh/parkpark/Registro.java
@@ -1,7 +1,9 @@
 package com.example.eddyh.parkpark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+
+    }
+
+    public void registrar(){
+
+    }
+    public void cancelar(View sender){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
